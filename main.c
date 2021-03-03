c06e9e4d0a13ca4d58ddebb64a0ad202df03b3f2
Michael Vassernis <michael.vassernis@tandemg.com>
2021-03-03 16:00:45 +0200

    simple main + makefile
    Signed-off-by: Michael Vassernis <michael.vassernis@tandemg.com>

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argv, char *argc[])
+{
+	printf("mem_tester - start\n");
+	printf("mem_tester - generating random data\n");
+	printf("mem_tester - write random data\n");
+	printf("mem_tester - read random data\n");
+
+	return 0;
+}
+
